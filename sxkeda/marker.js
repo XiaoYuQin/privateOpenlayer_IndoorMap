85fc212355dd14eb5c67b2069f3159a8e29a9061
秦晓宇 <qinxiaoyu@163.com>
2016-08-01 10:04:13 +0800

    添加一个marker到地图上

---
+++ sxkeda/marker.js
@@ -0,0 +1,10 @@
+function createStyle(src, img) {
+    return new ol.style.Style({
+        image : new ol.style.Icon(/** @type {olx.style.IconOptions} */( {
+            anchor : [0.5, 0.96],
+            src : src,
+            img : img,
+            imgSize : img ? [img.width, img.height] : undefined
+        }))
+    });
+}
